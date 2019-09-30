4d2c0f6f731ef0880a9331b7fef2b2293cd0906c
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2019-09-30 12:41:15 -0400

    Example screenshot

--- app.R
+++ app.R
@@ -2,7 +2,8 @@ library(shiny)
 
 # Define UI for App
 ui <- fluidPage(
-
+  includeCSS("https://educationshinyappteam.github.io/Style_Guide/css/style.css"),
+  headerPanel("New Application")
 )
 
 # Define server logic
